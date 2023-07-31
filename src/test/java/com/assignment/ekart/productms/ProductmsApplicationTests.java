46c0425bba61b9a7ba3f47de7f22f041100ff1a5
unknown <2022mt93183@wilp.bits-pilani.ac.in>
2023-07-31 22:58:40 +0530

    project added

---
+++ src/test/java/com/assignment/ekart/productms/ProductmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.ekart.productms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
