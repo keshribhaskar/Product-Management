fa9d8e0a7a0d4625ae20b14e2d3191b48a4b13b9
unknown <2022mt93183@wilp.bits-pilani.ac.in>
2023-10-18 19:19:18 +0530

    initial product service commit

---
+++ src/main/java/com/assignment/ekart/productms/controller/ProductController.java
@@ -0,0 +1,44 @@
+package com.assignment.ekart.productms.controller;
+
+import com.assignment.ekart.productms.model.ProductDetails;
+import com.assignment.ekart.productms.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/productApi")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping(value = "/product")
+    public ResponseEntity<List<ProductDetails>> getAllProducts() throws Exception {
+        List<ProductDetails> productDetails = productService.getAllProducts();
+        return new ResponseEntity<>(productDetails, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/product/{productId}")
+    public ResponseEntity<ProductDetails> getProductById(@PathVariable Integer productId) throws Exception {
+        ProductDetails productDetails = productService.getProductById(productId);
+        return new ResponseEntity<>(productDetails, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/addProduct")
+    public ResponseEntity<String> addNewProduct(@Valid @RequestBody ProductDetails product) throws Exception {
+        //Write your logic here
+        String message = productService.addProduct(product);
+        return new ResponseEntity<>(message, HttpStatus.OK);
+
+    }
+
+}
